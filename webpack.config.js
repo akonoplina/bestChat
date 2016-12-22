f26278d8c2f655742004938080074470c0f32376
anastacia <anastacia.cannabis@gmail.com>
2016-12-22 13:45:14 +0300

    Some old stuff & things that will be used later in project

---
+++ webpack.config.js
@@ -0,0 +1,52 @@
+var path = require('path');
+var webpack = require('webpack');
+var NpmInstallPlugin = require('npm-install-webpack-plugin');
+// var autoprefixer = require('autoprefixer');
+// var precss = require('precss');
+
+module.exports = {
+    devtool: 'cheap-module-eval-source-map',
+    entry: [
+        'webpack-hot-middleware/client',
+        'babel-polyfill',
+        './src/index'
+    ],
+    output: {
+        path: path.join(__dirname, 'dist'),
+        filename: 'bundle.js',
+        publicPath: '/static/'
+    },
+    plugins: [
+        new webpack.optimize.OccurenceOrderPlugin(),
+        new webpack.HotModuleReplacementPlugin(),
+        new NpmInstallPlugin()
+    ],
+    module: {
+        preLoaders: [
+            {
+                test: /\.js$/,
+                loaders: ['eslint'],
+                include: [
+                    path.resolve(__dirname, "src"),
+                ],
+            }
+        ],
+        loaders: [
+            {
+                loaders: ['react-hot', 'babel-loader'],
+                include: [
+                    path.resolve(__dirname, "src"),
+                ],
+                test: /\.js$/,
+                plugins: ['transform-runtime'],
+            },
+            {
+                test: /\.css$/,
+                loader: 'style!css-loader?modules&importLoaders=1&localIdentName=[name]__[local]___[hash:base64:5]'
+            }
+        ]
+    },
+    // postcss: function () {
+    //     return [autoprefixer, precss];
+    // }
+};
